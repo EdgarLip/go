df2f78dc72e450d108a3fa580e5f07b073d7de6b
lipnitsk <edgar.lipnitsky@nokia.com>
2024-03-12 17:39:15 +0200

    basic ma check

--- proj-test-1/main.go
+++ proj-test-1/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 
+	mapsfunctions "proj-test-1/mapsfunctions"
 	utilsfunctions "proj-test-1/utilsfunctions"
 )
 
 func main() {
 	fmt.Printf("inside pakcage main-> in main function this is where the go starts !\n")
 	utilsfunctions.Print_package_name()
-	utilsfunctions.Print_file_name()
-	utilsfunctions.Print_file_name2()
-	utilsfunctions.Print_utils_test_var() // just testing comments here !!!??
+	// utilsfunctions.Print_file_name()
+	// utilsfunctions.Print_file_name2()
+	// utilsfunctions.Print_utils_test_var() // just testing comments here !!!??
+	// utilsfunctions.Check_assimernt_if()
+	//------maps ------
+	mapsfunctions.Basic_map()
+
 }
--- proj-test-1/utilsfunctions/utilsfunctions.go
+++ proj-test-1/utilsfunctions/utilsfunctions.go
@@ -13,3 +13,11 @@ func Print_file_name() {
 func Print_utils_test_var() {
 	fmt.Printf("utils_test_var = %d\n", Utils_helper_var)
 }
+
+func Check_assimernt_if() {
+	if x := 10; x > 12 {
+		fmt.Printf("x = %d, we are in the if statment !!! \n", x)
+	} else {
+		fmt.Printf("x = %d, we are in else  statment !!! \n", x)
+	}
+}
