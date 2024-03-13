ea391b9c7c2bbbdf719f33c34c6579ba851d1e1a
lipnitsk <edgar.lipnitsky@nokia.com>
2024-03-13 22:10:36 +0200

    last-one-for-today

--- proj-test-1/main.go
+++ proj-test-1/main.go
@@ -12,8 +12,8 @@ import (
 
 func main() {
 	fmt.Printf("inside pakcage main-> in main function this is where the go starts !\n")
+	//--- import package in same module ---
 	utilsfunctions.Print_package_name()
-	//--- random stuff ---
 	// utilsfunctions.Print_file_name()
 	// utilsfunctions.Print_file_name2()
 	// utilsfunctions.Print_utils_test_var() // just testing comments here !!!??
@@ -21,7 +21,7 @@ func main() {
 	//------maps ------
 	mapsfunctions.Basic_map()
 
-	//--- import packages ---
+	//--- import module ---
 	//fmt.Printf("other package name = %s", pt2helperfunctions.Get_package_name())
 	fmt.Println(mygoutil.Get_package_name())
 
